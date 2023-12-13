cca9bd9f0e86a604546ddd237a2d742de5819d0a
rick-n-shawty <sultanhoodie@gmail.com>
2023-12-13 13:19:56 +0800

    path fix

--- routes/Cards.js
+++ routes/Cards.js
@@ -2,7 +2,7 @@ import createCard from "../controllers/Cards/CreateCard.js";
 import getCards from "../controllers/Cards/GetCards.js";
 import express from "express"; 
 import data from "../imports.cjs";
-import getCard from "../controllers/Cards/getCard.js";
+import getCard from "../controllers/Cards/GetCard.js";
 import deleteCard from "../controllers/Cards/DeleteCard.js";
 import auth from "../controllers/Middleware/auth.js";
 const { upload } = data;
@@ -12,4 +12,5 @@ router.post("/upload", [ auth, upload.array("files") ], createCard);
 router.get("/all/:type", getCards);
 router.get("/:id", getCard);
 router.delete("/:id", auth, deleteCard); 
+
 export default router; 
